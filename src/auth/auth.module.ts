a0c2a17ef339d69c2087438d2eb862d24b041b16
alexandre-dev28 <axel.business29@gmail.com>
2024-08-21 12:51:53 +0200

    Add JWT configuration and LoginCommandHandler
    Integrate JWT setup with dynamic configuration for token handling within the Auth module. Additionally, include LoginCommandHandler in the list of command handlers to support login functionality.
    
    Signed-off-by: alexandre-dev28 <axel.business29@gmail.com>

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
 import { authCommandHandlers, authQueriesHandler } from './exporter';
 import { AuthService } from './Common/services/auth.service';
-import { PrismaService } from 'nestjs-prisma';
 import { AuthController } from './Controllers/auth.controller';
 import { CqrsModule } from '@nestjs/cqrs';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { PrismaService } from '@/music_sheet/prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [CqrsModule],
+  imports: [
+    CqrsModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          global: true,
+          secret: configService.get<string>('JWT_SECRET_SECRET_KEY'),
+          signOptions: { algorithm: 'HS256', expiresIn: '1d' },
+        };
+      },
+    }),
+  ],
   providers: [
     ...authCommandHandlers,
     ...authQueriesHandler,
--- src/auth/exporter.ts
+++ src/auth/exporter.ts
@@ -1,7 +1,9 @@
 import { CommandHandlerType, QueryHandlerType } from '@nestjs/cqrs';
 import { RegisterCommandHandler } from './Commands/Register/RegisterCommandHandler';
+import { LoginCommandHandler } from '@/music_sheet/auth/Commands/Login/LoginCommandHandler';
 
 export const authCommandHandlers: Array<CommandHandlerType> = [
   RegisterCommandHandler,
+  LoginCommandHandler,
 ];
 export const authQueriesHandler: Array<QueryHandlerType> = [];
